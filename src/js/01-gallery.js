2bb63303374d0d71da68e80dde5048e170e79348
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-03-12 23:27:39 +0200

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "SimpleLightbox"
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryMarkup = handelGalleryMarkup(galleryItems);
+
+function handelGalleryMarkup(items) {
+    return items.map(item => 
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" 
+          data-source="${item.original}" alt="${item.alt}">
+        </a>
+      </div>`
+    )
+    .join('');
+
+}
+
+var lightbox = new SimpleLightbox (".gallery a", {
+     captionSelector: 'img',
+     captionsData: 'alt',
+     captionPosition: 'bottom',
+     captionDelay: 250,
+     scrollZoom: false,
+    });
+
+
