9b1d1133ee3a4f9956c10fff9de2e920c2e75964
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-03-12 23:32:07 +0200

    gallery-items

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,7 +6,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
 
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
