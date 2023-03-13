9ce1fc4cc9e7767c5c65269cf2a5237850f20a36
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-03-13 10:15:23 +0200

    Update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player('iframe');
+
+player.on('timeupdate', throttle(function(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}, 1000));
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+const time = JSON.parse(correntTime);
+
+player.setCurrentTime(time.seconds || 0);
