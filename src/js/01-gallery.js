da58bc9f52125115c97c4e9f79297db9cc517020
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-03-13 09:21:29 +0200

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,47 +6,46 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryList = document.querySelector('.gallery');
 
 const galleryMarkup = handelGalleryMarkup(galleryItems);
 
 function handelGalleryMarkup(items) {
-return items
-.map(
-(item) =>
-<div class="gallery__item"> <a class="gallery__item" href="${item.original}"> <img class="gallery__image" src="${item.preview}" alt="${item.description}" /> </a> </div>
-)
-.join('');
+    return items.map(item => 
+        `<div class="gallery__item">
+        <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+        </div>`
+    )
+    .join('');
+
 }
 
-galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
-
-galleryList.addEventListener('click', handelGalleryClick);
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup)
+galleryList.addEventListener('click', handelGalleryClick)
 
 function handelGalleryClick(event) {
-event.preventDefault();
+    event.preventDefault()
 
-if (event.target.nodeName !== 'IMG') {
-return;
-}
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
 
-const lightbox = new SimpleLightbox('.gallery a', {
-captionSelector: 'img',
-captionsData: 'alt',
-captionPosition: 'bottom',
-captionDelay: 250,
-scrollZoom: false,
-});
+    new SimpleLightbox(".gallery a", {
+     captionSelector: 'img',
+     captionsData: 'alt',
+     captionPosition: 'bottom',
+     captionDelay: 250,
+     scrollZoom: false,
+    });
 
-lightbox.open();
+    const modalImg = event.target.dataset.source;
 
-const modalImg = event.target.dataset.source;
+    const modalImgShow = basicLightbox.create(
+        `<img src="${modalImg}" width="800" height="600">`
+    )
+    
+    modalImgShow.show()
 
-const modalImgShow = basicLightbox.create(
-<img src="${modalImg}" width="800" height="600">
-);
-
-modalImgShow.show();
 }
