14380b86e38315485c7fd9cc5b1885feddee358f
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-03-13 09:22:07 +0200

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,6 +6,8 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
 
+console.log(galleryItems);
+
 const galleryList = document.querySelector('.gallery');
 
 const galleryMarkup = handelGalleryMarkup(galleryItems);
